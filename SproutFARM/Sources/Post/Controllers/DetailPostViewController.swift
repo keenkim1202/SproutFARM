935ebce5175816f86bf82fc16fada828beb6c117
KEEN <homurahomu@naver.com>
2022-01-13 01:06:33 +0900

    불필요 코드 제거

--- SproutFARM/Sources/Post/Controllers/DetailPostViewController.swift
+++ SproutFARM/Sources/Post/Controllers/DetailPostViewController.swift
@@ -15,7 +15,6 @@ class DetailPostViewController: BaseViewController {
   }
   
   // MARK: - Properties
-  // let minimumHeight: CGFloat = 80
   var keyHeight: CGFloat?
   var user: User?
   var post: Post?
@@ -169,15 +168,6 @@ extension DetailPostViewController: UITableViewDelegate, UITableViewDataSource {
       return cell
     }
   }
-  
-  // func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-  //   if indexPath.row == 1 { // post content
-  //     print(UITableView.automaticDimension)
-  //     return (UITableView.automaticDimension < minimumHeight) ? UITableView.automaticDimension : minimumHeight
-  //   } else {
-  //     return UITableView.automaticDimension
-  //   }
-  // }
   
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     if indexPath.row > 2 {
